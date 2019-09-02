3bbb4093a69d976a390148f20895fe4902e4eb88
mlhomfeld <mlhomfeld@yahoo.com>
2019-09-02 11:15:38 -0500

    Removing extra donation option buttons

--- src/components/DonateBtn.js
+++ src/components/DonateBtn.js
@@ -2,13 +2,7 @@ import React from "react"
 import buttonStyle from "./donatebtn.module.css"
 
 export default ({children}) => (
-  <div class="buttons has-addons navbar-item">
-    <span className={buttonStyle.donatebutton}
-      class="button"><a href={children}>$1</a></span>
-    <span className={buttonStyle.donatebutton}
-       class="button"><a href={children}>$5</a></span>
-    <span className={buttonStyle.donatebutton}
-       class="button"><a href={children}>$__</a></span>
+  <div class="buttons navbar-item">
     <span className={buttonStyle.donatebutton}
        class="button"><a href={children}>Donate</a></span>
   </div>
