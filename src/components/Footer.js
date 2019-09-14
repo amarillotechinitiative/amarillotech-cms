0af9a63b7abf95a8236d59256b821d3dcd1c38e9
Cailynn White <Cailynn888@gmail.com>
2019-09-14 18:30:24 -0500

    Update Footer.js
    Removed extra inline styles and menu lists

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-text-white-ter">
-        <div className="content has-text-centered has-text-white-ter">
+        <div className="content has-text-white-ter">
           <div className="container has-text-white-ter">
             <div className="columns">
 				<div className="column is-4 social">
@@ -18,7 +18,6 @@ const Footer = class extends React.Component {
                   <img
                     src={facebook}
                     alt="Facebook"
-                    style={{ width: '3em', height: '3em' }}
                   />
                 </a>
                 <a title="meetup" href="https://meetup.com">
@@ -26,68 +25,20 @@ const Footer = class extends React.Component {
                     className="fas fa-lg"
                     src={meetup}
                     alt="Amarillo Tech Meetup"
-                    style={{ width: '3em', height: '3em' }}
                   />
                 </a>
                 <a title="email" href="https://instagram.com">
                   <img
                     src={email}
                     alt="E-mail"
-                    style={{ width: '3em', height: '3em' }}
                   />
                 </a>
               </div>
               <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
+		
               </div>
               <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
+
               </div>
             </div>
           </div>
