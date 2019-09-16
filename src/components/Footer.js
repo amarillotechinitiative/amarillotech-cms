1b50c7a26725e86bf40403fb8c730663f45a1304
Cailynn White <34075271+Cailynn888@users.noreply.github.com>
2019-09-16 12:41:05 -0500

    Update Footer.js
    Added Links to Home/About/Blog/Contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,36 +12,59 @@ const Footer = class extends React.Component {
       <footer className="footer has-text-white-ter">
         <div className="content has-text-white-ter">
           <div className="container has-text-white-ter">
-            <div className="columns">
+	    <div className="columns">
 	    	<div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                  />
-                </a>
-                <a title="meetup" href="https://meetup.com">
-                  <img
-                    className="fas fa-lg"
-                    src={meetup}
-                    alt="Amarillo Tech Meetup"
-                  />
-                </a>
-                <a title="email" href="https://instagram.com">
-                  <img
-                    src={email}
-                    alt="E-mail"
-                  />
-                </a>
-              </div>
-              <div className="column is-4">
-    		<p>Input For Email</p>
+			<a title="facebook" href="https://facebook.com">
+				  <img
+				    src={facebook}
+				    alt="Facebook"
+				  />
+			</a>
+			<a title="meetup" href="https://meetup.com">
+				  <img
+				    className="fas fa-lg"
+				    src={meetup}
+				    alt="Amarillo Tech Meetup"
+				  />
+			</a>
+			<a title="email" href="https://instagram.com">
+				  <img
+				    src={email}
+				    alt="E-mail"
+				  />
+			</a>
+              	</div>
+	    	<div className="column is-4">
+	    		
 	    	</div
-
-              <div className="column is-4">
-	    	<p>Links</p>
-	    	</div>
-            </div>
+	    	<div className="column is-4">
+	    		<section className="menu">
+	    			<ul className="menu-list">
+	    				<li>
+	    					<Link to="/" className="navbar-item">Home</Link>
+	    				</li>
+						<li>
+							<Link className="navbar-item" to="/blog">Blog</Link>
+						</li>
+	    				<li>
+			      			<Link className="navbar-item" to="/about">About Us</Link>
+						</li>
+						<li>
+							<a
+								className="navbar-item"
+								href="/admin/"
+								target="_blank"
+								rel="noopener noreferrer" >Admin</a>
+						</li>
+						<li>
+							<Link className="navbar-item" to="/contact">Contact</Link>
+						</li>
+					</ul>
+				</section>
+			  </div>
+		<div className="column is-4">
+		</div>
+			</div>
           </div>
         </div>
       </footer>
