9f5b052cef6da3e386920d43ab47d0f81eff8c70
mlhomfeld <mlhomfeld@yahoo.com>
2019-09-26 20:03:54 -0500

    Create AboutBlurb.js

---
+++ src/components/AboutBlurb.js
@@ -0,0 +1,15 @@
+import React from "react"
+import logo from '../img/logo/VerticalLogo_White.svg'
+
+export default ({children}) => (
+  <div class="box">
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <img src={logo} />
+      </div>
+      <div class="column is-three-quarters">
+        {children}
+      </div>
+    </div>
+  </div>
+)
